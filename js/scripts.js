82d9925521c327d68f9ca987aa532c43fde9545c
stuartfreaks <stuartfreaks@me.com>
2022-07-11 16:45:10 +0200

    8 Pokemon created
    First 8 Pokemon created 3 keys with values

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+let pokemonList = [ {name: "Bulbasaur", height: 0.7, type: ["grass", "poison"]},
+                    {name: "Ivysaur", height: 1, type: ["grass", "poison"]},
+                    {name: "Venusaur", height: 2, type: ["grass", "poison"]},
+                    {name: "Charmander", height: 0.6, type: ["fire"]},
+                    {name: "Charmeleon", height: 1.1, type: ["fire"]},
+                    {name: "Charizard", height: 1.7, type: ["fire", "flying"]},
+                    {name: "Squirtile", height: 0.5, type: ["water"]},
+                    {name: "Wartortle", height: 1, type: ["water"]},
+
+
+
+
+
+]; 
