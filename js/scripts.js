28d212febe02b83c7f44b298dca0c31ae6f1aac0
stuartfreaks <stuartfreaks@me.com>
2022-07-17 11:08:55 +0200

    for Loop and height message added
    Pokemon are now output to the DOM

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,19 @@ let pokemonList = [ {name: "Bulbasaur", height: 0.7, type: ["grass", "poison"]},
                     {name: "Squirtile", height: 0.5, type: ["water"]},
                     {name: "Wartortle", height: 1, type: ["water"]},
 
-
-
-
-
 ]; 
+
+
+
+// Create a for loop that loops over pokemonList 
+
+for (let i=0; i<pokemonList.length; i++) {
+    
+// outputs name and height to the DOM plus writes message for pokemons with a height over 0.6 and adds message (but only to Venusaur)
+
+    if (pokemonList[i].height>1.7) {
+        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow! that\'s big!')
+    } else {
+        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')</p>')
+    }
+}
