674e8c461b713b49e13e40777aeef04c0948bd63
stuartfreaks <stuartfreaks@me.com>
2022-07-26 12:08:09 +0200

    IIFE added & Loop Updated
    IIFE added to the pokemon array & Loop Updated to deal with changes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,42 @@
-let pokemonList = [ {name: "Bulbasaur", height: 0.7, type: ["grass", "poison"]},
-                    {name: "Ivysaur", height: 1, type: ["grass", "poison"]},
-                    {name: "Venusaur", height: 2, type: ["grass", "poison"]},
-                    {name: "Charmander", height: 0.6, type: ["fire"]},
-                    {name: "Charmeleon", height: 1.1, type: ["fire"]},
-                    {name: "Charizard", height: 1.7, type: ["fire", "flying"]},
-                    {name: "Squirtile", height: 0.5, type: ["water"]},
-                    {name: "Wartortle", height: 1, type: ["water"]},
+//Pokemon array wrapped in IIFE
 
-]; 
+let pokemonRepository = (function () {
+
+   let pokemonList = [ {name: "Bulbasaur", height: 0.7, type: ["grass", "poison"]},
+                       {name: "Ivysaur", height: 1, type: ["grass", "poison"]},
+                       {name: "Venusaur", height: 2, type: ["grass", "poison"]},
+                       {name: "Charmander", height: 0.6, type: ["fire"]},
+                       {name: "Charmeleon", height: 1.1, type: ["fire"]},
+                       {name: "Charizard", height: 1.7, type: ["fire", "flying"]},
+                       {name: "Squirtile", height: 0.5, type: ["water"]},
+                       {name: "Wartortle", height: 1, type: ["water"]},
+   
+   ]; 
+   
+   return {
+       add: function(pokemon){
+           pokemonList.push(pokemon);
+       },
+       getAll: function() {
+         return pokemonList;
+    }   
+   
+    };
+   })();
+   
+   //getAll: return all items (pokemonRepository.getAll(); should return the pokemonList array)
+   
+   console.log(pokemonRepository.getAll()); //[]
+
+   //add: add a single item to the pokemonList array (calling pokemonRepository.add(item);
+   
+   pokemonRepository.add({name: 'Pikachu'});
+
+   console.log(pokemonRepository.getAll());
 
 
+   
 
-// Use a forEach() function instead of the for loop you have to iterate over the Pokémon in your pokemonList array in order to print the details of each one
-
-pokemonList.forEach(function(name) {
-   console.log(name);
-});
+   
+   
+   
